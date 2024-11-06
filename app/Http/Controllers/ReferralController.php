a30ad945b7cb4ecdf0fa2a8ea2678d375f38f46f
Malasuerte94 <ene.catalin.94@gmail.com>
2024-11-06 17:04:07 +0200

    Add referrals

--- app/Http/Controllers/ReferralController.php
+++ app/Http/Controllers/ReferralController.php
@@ -21,9 +21,14 @@ class ReferralController extends Controller
             return 'Nope!';
         }
 
+        $referral = Referral::where('user_id', $id)->where('referred_user', $referredUser)->first();
+        if ($referral) {
+            return 'Ai fost adus deja de ' . $referral->parent_user;
+        }
+
         $referral = Referral::where('user_id', $id)->where('parent_user', $parentUser)->where('referred_user', $referredUser)->first();
         if ($referral) {
-            return 'Deja este!';
+            return 'Nu poti de mai multe ori';
         }
 
         $referral = new Referral();
