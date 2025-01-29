57abbf20a6553ed7f35a30ad7185add883976888
Malasuerte94 <ene.catalin.94@gmail.com>
2025-01-29 11:42:42 +0200

    Fix add schedule

--- resources/js/Pages/SchedulePage.vue
+++ resources/js/Pages/SchedulePage.vue
@@ -84,6 +84,38 @@
                 </button>
               </div>
             </div>
+
+            <!-- Modals -->
+            <CreateScheduleModal v-if="showCreateModal" @close="closeAndRefresh()" />
+            <EditDayModal
+                v-if="showEditDayModal"
+                :day="selectedDay"
+                @close="showEditDayModal = false"
+                @updated="fetchSchedules"
+            />
+            <div
+                v-if="showDiscordModal"
+                class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50"
+            >
+              <div class="bg-white p-6 rounded-lg shadow-lg">
+                <h3 class="text-xl font-bold text-gray-700 mb-4">Announce to Discord</h3>
+                <p class="text-gray-600 mb-4">Are you sure you want to announce this schedule to Discord?</p>
+                <div class="flex gap-4 justify-end">
+                  <button
+                      @click="announceToDiscord()"
+                      class="px-4 py-2 bg-purple-600 text-white rounded-md shadow hover:bg-purple-500 focus:outline-none focus:ring focus:ring-purple-300"
+                  >
+                    Yes
+                  </button>
+                  <button
+                      @click="closeDiscordModal"
+                      class="px-4 py-2 bg-gray-300 text-gray-700 rounded-md shadow hover:bg-gray-200"
+                  >
+                    No
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
