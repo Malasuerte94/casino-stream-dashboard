140ccae5afb73c30047ef52cfc283457498f30de
Malasuerte94 <ene.catalin.94@gmail.com>
2025-02-24 12:07:58 +0200

    Add battle predictions - build

---
+++ public/build/assets/PrimaryButton-4bfa9edb.js
@@ -0,0 +1,1 @@
+import{o as r,c as o,r as s}from"./app-24ee4374.js";const a=["type"],c={__name:"PrimaryButton",props:{type:{type:String,default:"submit"}},setup(t){return(e,n)=>(r(),o("button",{type:t.type,class:"btn-primary"},[s(e.$slots,"default")],8,a))}};export{c as _};
